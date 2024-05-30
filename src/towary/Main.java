575d861426ce99101683fb9645a88c3f2d7916fa
Sajmon <linowski.szymon@gmail.com>
2024-05-30 09:58:32 +0200

    zaj2Szymon

--- src/towary/Main.java
+++ src/towary/Main.java
@@ -1,9 +1,28 @@
 package towary;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class Main {
     public static void main(String[] args) {
+        BlockingQueue<Item> queue = new ArrayBlockingQueue<>(100);
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+
+        Thread producerThread = new Thread(producer);
+        Thread consumerThread = new Thread(consumer);
+
+        producerThread.start();
+        consumerThread.start();
+
+        try {
+            producerThread.join();
+            consumerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
        // https://sdkp.pjwstk.edu.pl/html/staskshtml/S_TOWARY/S_TOWARY.html
 
-    }
-}
